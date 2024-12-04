bf0c8d45c62acdba690150229533389682cbec22
assan07 <hasanwkl04@gmail.com>
2024-12-05 00:24:51 +0800

    haduh ada yg bentrok wkwk

--- app.py
+++ app.py
@@ -33,7 +33,6 @@ app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
 # Home dan halaman about
 
 
